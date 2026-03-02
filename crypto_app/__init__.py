77176869d399e9af769139ee32a2c0530bcc02ab
igorul147-droid <igor_ul147@hotmail.com>
2026-03-02 11:01:41 -0300

    Create __init__.py

---
+++ crypto_app/__init__.py
@@ -0,0 +1,7 @@
+diff --git a/crypto_app/__init__.py b/crypto_app/__init__.py
+new file mode 100644
+index 0000000000000000000000000000000000000000..dfa2d9b46bea80ba84a15fbb474949f35fe9a1ca
+--- /dev/null
++++ b/crypto_app/__init__.py
+@@ -0,0 +1 @@
++"""Utilities for the crypto Streamlit app."""
